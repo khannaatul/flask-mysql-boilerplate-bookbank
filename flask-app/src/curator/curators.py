457a6602c855e69a6fb010810d69bb56ec0635f7
Keily <Keily-Northeastern@Keilys-MacBook-Air-2.local>
2023-04-18 22:38:36 -0400

    Curator routes

--- flask-app/src/curator/curators.py
+++ flask-app/src/curator/curators.py
@@ -3,14 +3,14 @@ import json
 from src import db
 
 
-bookreaders = Blueprint('bookreaders', __name__)
+curators = Blueprint('curators', __name__)
 
-# Get all bookreaders from the BookBank
-@bookreaders.route('/bookreaders', methods=['GET'])
+# Get all curators from the BookBank
+@curators.route('/curators ', methods=['GET'])
 def get_bookreaders():
     cursor = db.get_db().cursor()
-    cursor.execute('SELECT userid,\
-       first, last, email, username FROM BookReader')
+    cursor.execute('SELECT CuratorID,\
+       first, last, email, username FROM Curator')
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -21,11 +21,11 @@ def get_bookreaders():
     the_response.mimetype = 'application/json'
     return the_response
 
-# Get bookreader detail for bookreader with particular userid
-@bookreaders.route('/bookreaders/<userid>', methods=['GET'])
-def get_bookreader(userid):
+# Get curator detail for curator with particular curatorid
+@bcurators.route('/curators/<curatorid>', methods=['GET'])
+def get_curator(curatorid):
     cursor = db.get_db().cursor()
-    cursor.execute('select * from BookReader where id = {0}'.format(userid))
+    cursor.execute('select * from Curator where id = {0}'.format(userid))
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -38,22 +38,22 @@ def get_bookreader(userid):
 
 
 
-@ookreaders.route('/newbookreaders', methods = ['POST'])
-def add_bookreader():
+@curators.route('/newcurators', methods = ['POST'])
+def add_curator():
 
     the_data = request.json
     current_app.logger.info(the_data)
 
-    first = the_data['bookreader_first']
-    last = the_data['bookreader_last']
-    email = the_data['bookreader_email']
-    username = the_data['bookreader_username']
-    password = the_data['bookreader_password']
-    city = the data['bookreader_city']
-    state = the data['bookreader_state']
-    zip = the data['bookreader_zip']
+    first = the_data['curator_first']
+    last = the_data['curator_last']
+    email = the_data['curator_email']
+    username = the_data['curator_username']
+    password = the_data['curator_password']
+    city = the data['curator_city']
+    state = the data['curator_state']
+    zip = the data['curator_zip']
 
-    query = 'Insert into bookreader (first, last, email, username, password, city, state, zip) values(" '
+    query = 'Insert into curator (first, last, email, username, password, city, state, zip) values(" '
     query += first  + ' " , " ' 
     query += last + ' " , ' 
     query += email + ' " , ' 
@@ -78,10 +78,10 @@ def add_bookreader():
 
 
 
-#delete bookreader
-@app.route("/bookreaders/<userid>", methods=["DELETE"])
-def bookreader_delete(userid):
-    bookreader = BookReader.query.get(userid)
-    db.session.delete(bookreader)
+#delete curator
+@app.route("/curatordelete/<curatorid>", methods=["DELETE"])
+def curator_delete(curatorid):
+    curator = Curator.query.get(curatorid)
+    db.session.delete(curator)
     db.session.commit()
-    return "BookReader was successfully deleted"
+    return "Curator was successfully deleted"
